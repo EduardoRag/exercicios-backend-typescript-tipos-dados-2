5e0e6ec09664fbd3f3ab26233c3b071bec19680b
EduardoRag <eduardojarek66@gmail.com>
2023-05-19 19:09:30 -0300

    Exercício 01 finalizado

---
+++ 01/scripts.ts
@@ -0,0 +1,33 @@
+import fs from 'fs';
+
+type user = {
+    name: string,
+    age: number,
+    status: 'active' | 'not active'
+}
+
+const writeFile = (user: user) => {
+    const jsonUser = JSON.stringify(user);
+
+    fs.writeFile('../bd.json', jsonUser, (error) => {
+        if (error) {
+            return console.log(error);
+        }
+    });
+}
+
+// writeFile({
+//     name: 'Eduardo',
+//     age: 26,
+//     status: 'active'
+// });
+
+const readFile = (): user => {
+    const fileData = fs.readFileSync('../bd.json', 'utf-8');
+
+    const userDb = JSON.parse(fileData);
+
+    return userDb;
+}
+
+console.log(readFile());
